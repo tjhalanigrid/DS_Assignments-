f8f61ef79a000d04830be3c139ec73f2d3205f8d
tjhalanigrid <tjhalani@griddynamics.com>
2026-01-29 12:03:59 +0530

    Add DS assignments (remove submodule)

---
+++ DS_Assignments-/DSA/Longest_Repeating_Character_Replacement.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        freq = [0] * 26
+        left = 0
+        max_freq = 0
+        ans = 0
+
+        for right in range(len(s)):
+            idx = ord(s[right]) - ord('A')
+            freq[idx] += 1
+            max_freq = max(max_freq, freq[idx])
+
+            while (right - left + 1) - max_freq > k:
+                freq[ord(s[left]) - ord('A')] -= 1
+                left += 1
+
+            ans = max(ans, right - left + 1)
+
+        return ans
